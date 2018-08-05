4b1b82b3bf4fe32840727af418c00c4d303e499f
Giuseppe Lippolis <giu.lippolis@gmail.com>
2018-08-05 15:29:03 +0200

    open_tunnel: bug fixed
    Signed-off-by: Giuseppe Lippolis <giu.lippolis@gmail.com>

--- owrt_scripts/sms_interpreter/open_ssh_tunnel.sh
+++ owrt_scripts/sms_interpreter/open_ssh_tunnel.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
-ssh -K30 -i /root/id_rsa_dropbearformat -R 2222:localhost:22 fakeuser@giuseppelippolis.dyndns.org
+k=0
+ret=1
+
+while [ "$k" -lt 10 -a "$ret" -ne 0 ]; do
+	ping -c5 giuseppelippolis.dyndns.org
+	ret=$?
+	k=$(( $k+1 ))
+	wait 10
+done
+
+
+logger -t rc.local "strting loop with ret $ret"
+
+if [ $ret -eq 0 ]; then
+	logger -t rc.local "strting ssh tunnell"
+	ssh -N -f -K30 -y -i /root/id_rsa_dropbearformat -R 2222:localhost:22 fakeuser@giuseppelippolis.dyndns.org 
+	logger -t rc.local "strted ssh tunnell"
+#else
+#	echo "no internet connection"
+fi
 
 #than in nitsche run ->  ssh root@localhost -p2222
+
